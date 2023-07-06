236f5291c3aa7777bd107af463106804b4fac9a3
Alex Tselegidis <alextselegidis@gmail.com>
2023-07-06 21:02:06 +0200

    Install Breeze

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,10 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
