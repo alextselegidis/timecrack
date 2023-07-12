00a8d930384591dac7c28f969a855913c2a2bc6f
Alex Tselegidis <alextselegidis@gmail.com>
2023-07-12 12:11:11 +0200

    Update the about page

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,23 +1,21 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2>
-            {{ __('about') }}
-        </h2>
+    <x-slot name="pageTitle">
+        {{ __('About') }}
     </x-slot>
 
     <div class="container">
         <div class="row">
             <div class="col">
-                <h6>
-                    Timecrack
-                </h6>
-
                 <p>
-                    Self hosted application for time tracking, allowing multiple users of a company to track their time easily.
+                    Self hosted application for time tracking, allowing multiple users of a company to track their time
+                    easily.
                 </p>
 
                 <div class="card">
                     <div class="card-body">
+                        <strong>
+                        {{__('Application Version')}}
+                        </strong>:
                         {{config('app.version')}}
                     </div>
                 </div>
