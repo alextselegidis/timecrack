0720ea8ec23c1cb19514e486d5b1fa1174b92e68
Alex Tselegidis <alextselegidis@gmail.com>
2023-11-08 11:10:49 +0100

    Correct typo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
 
         <!-- Remember Me -->
         <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" name="remembere" id="remember">
+            <input type="checkbox" class="form-check-input" name="remember" id="remember">
             <label class="form-check-label" for="remember">{{ __('rememberMe') }}</label>
         </div>
 
