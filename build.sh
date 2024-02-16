e4e0230cf95f3929c1e8959d90aae291a2b52b9c
Alex Tselegidis <alextselegidis@gmail.com>
2024-02-16 21:39:14 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
+# Dependencies
+
 composer install
+
 npm install
-find . -name ".DS_Store" -delete
+
+# Empty Storage
+
+find storage/app -type f ! -name '.gitignore' -exec rm -f {} \;
+
+# Clear Cache
+
+php artisan cache:clear
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
+php artisan clear-compiled
+
+# Remove Various
+
 rm public/hot
+
+find . -name ".DS_Store" -delete
+
+# Build Assets
+
 npm run build
+
+# Zip Files
+
 zip -r timecrack-0.0.0.zip . \
-    -x .git/**\* \
-    -x .idea/**\* \
-    -x tests/**\* \
-    -x .editorconfig \
-    -x .gitattributes \
-    -x .phpstorm.meta.php \
-    -x .prettierignore \
-    -x package-lock.json \
-    -x phpunit.xml \
-    -x _ide_helper.php \
-    -x vite.config.js \
-    -x \*.zip
+    -x '.git/*' \
+    -x '.idea/*' \
+    -x '.run/*' \
+    -x 'docker/*' \
+    -x 'node_modules/*' \
+    -x 'tests/*' \
+    -x '.editorconfig' \
+    -x '.gitattributes' \
+    -x '.gitignore' \
+    -x '.prettierignore' \
+    -x '.package-lock.json' \
+    -x 'build.sh' \
+    -x 'docker-compose.yml' \
+    -x 'postcss.config.js' \
+    -x 'tailwind.config.js' \
+    -x 'vite.config.js' \
+    -x '\*.zip'
