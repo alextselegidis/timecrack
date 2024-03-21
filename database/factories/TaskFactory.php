ef75340da78b8ae7b1e7a4d7b7e6f26428448549
Alex Tselegidis <alextselegidis@gmail.com>
2024-03-21 22:58:10 +0000

    Add values to the TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use DateInterval;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -13,11 +14,22 @@ class TaskFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @throws \Exception
      */
     public function definition(): array
     {
+        $randomStartedAt = $this->faker->dateTime();
+        $randomDuration = $this->faker->randomElement([15, 30, 60, 90]);
+        $randomDurationIntervalObject =new DateInterval('PT' . $randomDuration . 'M');
+        $randomEndedAt = clone $randomStartedAt;
+        $randomEndedAt->add($randomDurationIntervalObject);
+
         return [
-            //
+            'project_id' => null,
+            'user_id' => null,
+            'started_at' => $randomStartedAt,
+            'ended_at' => $randomEndedAt,
+            'summary' => $this->faker->sentence(),
         ];
     }
 }
