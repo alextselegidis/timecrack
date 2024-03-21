2d10adc28183c1821d4fdeabc30b3642d3589c4e
Alex Tselegidis <alextselegidis@gmail.com>
2024-03-21 22:36:39 +0000

    Narrower profile form

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,7 +5,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-md-6 offset-md-3">
                 <div>
                     <div>
                         <div>
